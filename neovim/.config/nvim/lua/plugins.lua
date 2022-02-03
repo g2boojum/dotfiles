29212d70d7bd185e92464f011213f3d05a5c1b11
Grant Goodyear <grant.goodyear@corelab.com>
2022-02-03 17:38:43 -0600

    nvim lua stuff

---
+++ neovim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,133 @@
+ local M = {}
+
+function M.setup()
+  -- Indicate first time installation
+  local packer_bootstrap = false
+
+  -- packer.nvim configuration
+  local conf = {
+    display = {
+      open_fn = function()
+        return require("packer.util").float { border = "rounded" }
+      end,
+    },
+  }
+
+  -- Check if packer.nvim is installed
+  -- Run PackerCompile if there are changes in this file
+  local function packer_init()
+    local fn = vim.fn
+    local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+    if fn.empty(fn.glob(install_path)) > 0 then
+      packer_bootstrap = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+      }
+      vim.cmd [[packadd packer.nvim]]
+    end
+    vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+  end
+
+  -- Plugins
+  local function plugins(use)
+    use { "wbthomason/packer.nvim" }
+
+    -- Colorscheme
+    use {
+      "sainnhe/everforest",
+      config = function()
+        vim.cmd "colorscheme everforest"
+      end,
+    }
+
+		-- Better comments
+		use {
+			"numToStr/Comment.nvim",
+			opt = true,
+			config = function()
+				require("Comment").setup {}
+			end,
+		}
+
+		-- treesitter
+		use {
+			"nvim-treesitter/nvim-treesitter",
+			run = ":TSUpdate",
+			config = function()
+				require("config.treesitter").setup()
+			end,
+		}
+
+		-- Git
+		use {
+			"TimUntersberger/neogit",
+			cmd = "Neogit",
+			requires = "nvim-lua/plenary.nvim",
+			config = function()
+				require("config.neogit").setup()
+			end,
+		}
+
+		-- WhichKey
+		use {
+			"folke/which-key.nvim",
+			config = function() 
+				require("config.whichkey").setup()
+			end,
+		}
+
+		-- IndentLine
+		use {
+			"lukas-reineke/indent-blankline.nvim",
+			config = function() 
+				require("config.indentblankline").setup()
+			end,
+		}
+
+		-- better icons
+   use {
+      "kyazdani42/nvim-web-devicons",
+      module = "nvim-web-devicons",
+      config = function()
+        require("nvim-web-devicons").setup { default = true }
+      end,
+    }
+
+		-- nvim-gps
+		use {
+			"SmiteshP/nvim-gps",
+			requires = "nvim-treesitter/nvim-treesitter",
+			module = "nvim-gps",
+			config = function()
+				require("nvim-gps").setup()
+			end,
+		}
+
+		-- lualine
+		use {
+			"nvim-lualine/lualine.nvim",
+			event = "VimEnter",
+			config = function()
+				require("config.lualine").setup()
+			end,
+			requires = { "nvim-web-devicons" },
+		}
+
+    if packer_bootstrap then
+      print "Restart Neovim required after installation!"
+      require("packer").sync()
+    end
+  end
+
+  packer_init()
+
+  local packer = require "packer"
+  packer.init(conf)
+  packer.startup(plugins)
+end
+
+return M
