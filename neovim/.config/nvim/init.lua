88b792c5ba0c4b30e58c84329d15bd04ec615b57
Grant Goodyear <grant.goodyear@corelab.com>
2022-02-08 16:40:19 -0600

    nvim update

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- Settings lifted from agriffis
+-- The utils and plugins have heavily been lifted from
+--   https://github.com/alpha2phi/neovim-for-beginner,
+-- with an assist from https://codevion.github.io/#!vim/treelsp.md
 require "settings"
 require "utils"
 require("plugins").setup()
