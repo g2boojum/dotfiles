20467d77cf7b80aa8299f5d79612af0fa9141f94
Grant Goodyear <grant.goodyear@corelab.com>
2024-05-31 16:14:28 -0500

    nvim config update continues

--- neovim/.config/nvim/lua/plugins/colorschemes.lua
+++ neovim/.config/nvim/lua/plugins/colorschemes.lua
@@ -2,4 +2,17 @@ return {
 	"folke/tokyonight.nvim",
 	"projekt0n/github-nvim-theme",
 	{ "rose-pine/neovim", name = "rose-pine" },
+	{
+		"capppucin/nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		name = "catppuccin",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- Load the colorscheme here
+			vim.cmd.colorscheme("catppuccin")
+
+			-- You can configure highlights by doing something like
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
 }
