eb15c20cc0ee096b2fee5735486a08e8f92d30ca
Grant Goodyear <grant.goodyear@bakerhughes.com>
2024-07-12 15:26:59 -0500

    fix oops

--- neovim/.config/nvim/plugin/keymaps.lua
+++ neovim/.config/nvim/plugin/keymaps.lua
@@ -8,8 +8,8 @@ vim.opt.hlsearch = true
 set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-set("n", "[d", vim.diagnostic.jump, { desc = "Go to previous [D]iagnostic message", count = -1, float = true })
-set("n", "]d", vim.diagnostic.jump, { desc = "Go to next [D]iagnostic message", count = 1, float = true })
+set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
+set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
